b96d86b890b7bdab2cc729eec7509b50abc1290a
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-01-31 19:12:47 +0000

    Activity 4: lots of prop drilling to set favourites

--- src/components/character_container.tsx
+++ src/components/character_container.tsx
@@ -1,46 +1,58 @@
-import React from 'react';
-import { DisneyCharacter } from '../disney_character';
-import Character from './character';
+import React from "react";
+import { DisneyCharacter } from "../disney_character";
+import Character from "./character";
+
+// character_container.tsx
+interface CharacterContainerProps {
+  characters: Array<DisneyCharacter>;
+  characterFavourites: Array<number>;
+  updateFavourites: (favourites: Array<number>) => void;
+}
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - an array of DisneyCharacter
-const CharacterContainer : React.FC<{ characters: Array<DisneyCharacter>}> = ( { characters}) => {
+const CharacterContainer: React.FC<CharacterContainerProps> = ({
+  characters,
+  characterFavourites,
+  updateFavourites,
+}) => {
+  // this function separates our array of DisneyCharacters into rows and columns
+  const buildRows = () => {
+    // we'll need arrays to store the rows and cols in, and they will be of type JSX.Element
+    let rows: Array<JSX.Element> = [],
+      cols: Array<JSX.Element> = [];
 
-	// this function separates our array of DisneyCharacters into rows and columns
-    const buildRows = () => {
-        
-		// we'll need arrays to store the rows and cols in, and they will be of type JSX.Element
-		let rows : Array<JSX.Element> = [], cols : Array<JSX.Element> = [];
-        
-		characters.forEach((character, index) => {
-            cols.push(<Character key={character._id} character={character} />);
-            if ((index + 1) % 5 === 0) {
-                rows.push(
-                    <div className="character-row" key={index}>
-                        {cols}
-                    </div>
-                )
-                cols = []
-            }
-        });
+    characters.forEach((character, index) => {
+        cols.push(
+            <Character key={character._id}
+                        character={character}
+                        characterFavourites={characterFavourites}
+                        updateFavourites={updateFavourites}
+            />
+        );
+      if ((index + 1) % 5 === 0) {
+        rows.push(
+          <div className="character-row" key={index}>
+            {cols}
+          </div>
+        );
+        cols = [];
+      }
+    });
 
-        // Final remaining columns
-        if (cols.length > 0) {
-            rows.push(
-                <div className="character-row" key={characters.length}>
-                    {cols}
-                </div>
-            )
-        }
-
-        return rows;
+    // Final remaining columns
+    if (cols.length > 0) {
+      rows.push(
+        <div className="character-row" key={characters.length}>
+          {cols}
+        </div>
+      );
     }
 
-    return (
-        <div className="character-container">
-            {buildRows()}
-        </div>
-    )
-}
+    return rows;
+  };
+
+  return <div className="character-container">{buildRows()}</div>;
+};
 
 export default CharacterContainer;
